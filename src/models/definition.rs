57af84dc5f1cf2bd2462f63bcf8cb365545da0a8
DestinyofYeet <ole@ole.blue>
2026-05-18 15:23:38 +0200

    chore(models): removed deprecated modelmigration struct

--- src/models/definition.rs
+++ src/models/definition.rs
@@ -27,20 +27,6 @@ pub enum ModelIteration {
     Modify(Vec<ModifyColumn>),
 }
 
-pub struct ModelMigration {
-    pub(crate) model_name: String,
-    pub(crate) data: Vec<ModelIteration>,
-}
-
-impl ModelMigration {
-    pub fn new(model_name: impl ToString, data: Vec<ModelIteration>) -> Self {
-        Self {
-            model_name: model_name.to_string(),
-            data,
-        }
-    }
-}
-
 pub trait Model {
     const TABLE_NAME: &'static str;
 
